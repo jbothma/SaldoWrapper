999e0384567b4de90ccf3f6d7726f57a79b789e5
JD Bothma <jbothma@gmail.com>
2012-03-31 18:28:51 +0200

    Only stderr is being dumped by separate thread, next step is query method.
    Assumes saldo output is line-buffered from here on.

--- src/uk/co/jbothma/saldo/Saldo.java
+++ src/uk/co/jbothma/saldo/Saldo.java
@@ -5,22 +5,29 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 public class Saldo {
 	private Process saldoProcess;
+	private Thread errorThread;
 
-	public Saldo(String binPath, String dictPath) throws IOException, InterruptedException {
+	public Saldo(String binPath, String dictPath) throws IOException,
+			InterruptedException {
 		ProcessBuilder pb = new ProcessBuilder(binPath, dictPath);
-		
-		pb.redirectErrorStream(true);
 		saldoProcess = pb.start();
 
-		new Thread(new Reader(saldoProcess.getInputStream())).start();
-		new Thread(new Writer(saldoProcess.getOutputStream())).start();
-		
-		saldoProcess.waitFor();
+		errorThread = new Thread(new ErrorReader(saldoProcess.getErrorStream()));
+		errorThread.start();
+
+		BufferedReader inputStream = new BufferedReader(new InputStreamReader(
+				saldoProcess.getInputStream()));
+		BufferedWriter outputStream = new BufferedWriter(
+				new OutputStreamWriter(saldoProcess.getOutputStream()), 100);
+
+		// test lookup
+		outputStream.write("ord\n\n");
+		outputStream.flush();
+		System.out.println(inputStream.readLine());
 	}
 
 	/**
@@ -30,14 +37,12 @@ public class Saldo {
 	 */
 	public static void main(String[] args) {
 		String binPath = "/home/jdb/uni/uppsala/2011-2012/thesis/sw_source/FM-SBLEX_svn/sblex/bin/saldo";
-		String dictPath = "/home/jdb/uni/uppsala/2011-2012/thesis/sw_source/FM-SBLEX_svn/dicts/saldo.dict";
+		String dictPath = "/home/jdb/uni/uppsala/2011-2012/thesis/sw_source/FM-SBLEX_svn/dicts/saldo100.dict";
 		try {
 			(new Saldo(binPath, dictPath)).close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -45,42 +50,26 @@ public class Saldo {
 	public void close() {
 		saldoProcess.destroy();
 	}
-	
-	class Reader implements Runnable {
+
+	class ErrorReader implements Runnable {
 		BufferedReader inputStream;
-		
-		Reader(InputStream inputStream) {
-			this.inputStream = new BufferedReader(new InputStreamReader(inputStream));
+
+		ErrorReader(InputStream inputStream) {
+			this.inputStream = new BufferedReader(new InputStreamReader(
+					inputStream));
 		}
-		
+
 		public void run() {
 			String line;
 			try {
 				while ((line = inputStream.readLine()) != null) {
-					System.out.println("reader thread " + line);
+					System.err.println("reader thread " + line);
 				}
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				// closing the file handle when the process exits throws an
+				// exception here. don't care enough about stderr to handle this
+				// better yet.
 			}
 		}
-	}
-	
-	class Writer implements Runnable {
-		BufferedWriter outputStream;
-		Writer(OutputStream outputStream) {
-			this.outputStream = new BufferedWriter( new OutputStreamWriter( outputStream ), 50 /* keep small for tests */ );
-		}
-		@Override
-		public void run() {
-			try {
-				outputStream.write("ord" + System.getProperty("line.separator"));
-				outputStream.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
 	}
 }
